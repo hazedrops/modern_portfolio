467870f816cc5ace6a73f7ea114426393eb24990
hazedrops <hazedrops@hotmail.com>
2021-05-17 21:08:57 -0700

    Modified work.html file to be dynamically generated

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,44 @@
+// import { createProjectDiv } from "./projects.js";
+
 // Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
+
+// const projects = document.querySelector(".projects");
+
+// const projectItems = [
+//   {
+//     pageAddr : "./mealFinder.html",
+//     imgSrc  : "./img/projects/p141.png",
+//     imgAlt     : "Meal Finder Project",
+//     title  : "Meal Finder",
+//     gitAddr : "https://github.com/hazedrops/SASS_API_MealFinder"
+//   },
+//   {
+//     pageAddr : "./weatherApp.html",
+//     imgSrc  : "./img/projects/p151.png",
+//     imgAlt     : "The Weather App Project",
+//     title  : "Weather App",
+//     gitAddr : "https://github.com/hazedrops/SASS_JS_WeatherApp"
+//   },
+//   {
+//     pageAddr : "./netflixClone.html",
+//     imgSrc  : "./img/projects/p181.png",
+//     imgAlt     : "Netflix Landing Page Clone",
+//     title  : "Netflix Page Clone",
+//     gitAddr : "https://github.com/hazedrops/CSS_NetflixClone"
+//   },
+//   {
+//     pageAddr : "./textReader.html",
+//     imgSrc  : "./img/projects/p231.png",
+//     imgAlt     : "Text Reader",
+//     title  : "Text Reader",
+//     gitAddr : "https://github.com/hazedrops/JS_CSS_SpeechTextReader"
+//   }
+// ];
 
 // Set Initial State Of Menu
 let showMenu = false;
@@ -31,3 +66,56 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// const createProjectDiv = (projects, projectItems) => {
+//   projectItems.forEach(project => {
+//     const item = document.createElement("div");  
+//     item.classList.add("item");
+
+//     const imgLink = document.createElement("a"); 
+//     imgLink.href = `${project.pageAddr}`;
+
+//     const img = document.createElement("img");
+//     img.src = project.imgSrc;
+//     img.alt = project.imgAlt;
+//     imgLink.append(img);
+
+//     const titleLink = document.createElement("a"); 
+//     titleLink.classList.add("btn-light");
+//     titleLink.target = "_blank";    
+
+//     const titleIcon = document.createElement("i");
+//     titleIcon.classList.add("fas");
+//     titleIcon.classList.add("fa-eye");
+//     titleLink.append(titleIcon);
+//     titleLink.innerText = ` ${project.title}`
+//     titleLink.href = `${project.pageAddr}`;
+
+//     const gitLink = document.createElement("a"); 
+//     gitLink.classList.add("btn-dark");
+//     gitLink.target = "_blank";   
+//     gitLink.href = `${project.gitAddr}`;
+
+//     const gitIcon = document.createElement("i");
+//     gitIcon.classList.add("fab");
+//     gitIcon.classList.add("fa-github");
+//     gitLink.append(gitIcon);
+//     gitLink.innerText = " Github",
+//     gitLink.href = `${project.gitAddr}`;  
+
+//     item.append(imgLink);
+//     item.append(titleLink);
+//     item.append(gitLink);
+
+//     projects.append(item);
+//   });
+
+//   return projects;
+// }
+
+// require("./projects.js")();
+// createProjectDiv();
+
+
+
+
